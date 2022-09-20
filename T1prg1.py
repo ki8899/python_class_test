7bcdd697b6de783a6648839a3a63e33de5789379
ki8899 <112924543+ki8899@users.noreply.github.com>
2022-09-20 09:22:48 +0530

    Add files via upload

---
+++ T1prg1.py
@@ -0,0 +1,25 @@
+#prg-1-check voting...
+# yr=int(input("Enter the age: "))
+# if(yr>=18):
+#     print("Eligible for voting....!")
+# else:
+#     print("Not Eligible for voting...! ")
+
+#prg-2-check leap yr
+#
+# year=int(input("Enter the year: "))
+# if(year%4==0 or year%400==0):
+#     print("Year is leap year...!")
+# else:
+#     print("Not a leap year...!")
+
+#prg-3-do factorial
+
+fact=1
+num=int(input("Enter the number: "))
+while num>0 :
+        fact=fact*num
+        num=num-1
+
+print("Factorial is: ",fact)
+
